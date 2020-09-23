d1c24de17845df0ecc28ebd43e038a6b5c2b7982
nick-sandifer <sandifer.ux@gmail.com>
2020-09-23 14:26:37 -0700

    make mobile hamburger menu (currently broken)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 
 
 console.log("Hey, welcome to Nick's portfolio site!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.classname === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
