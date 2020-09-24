1c557666f90cdbbc086f37b2470a00d7f28bf87e
nick-sandifer <sandifer.ux@gmail.com>
2020-09-24 16:55:43 -0700

    attempt to fix hamburger icon (the struggle is hitting hard)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
 
-
-console.log("Hey, welcome to Nick's portfolio site!");
-
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  x.className += ' responsive';
-}
-
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.classname === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += 'responsive';
   } else {
     x.className = 'navtoggle';
   }
